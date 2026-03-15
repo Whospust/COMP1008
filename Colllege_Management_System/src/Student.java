c997fdb6c86450de99b944c5787803d962b9af93
palexey2006 <palexey2006@gmail.com>
2026-03-15 15:23:26 -0400

    added regex validation inside Student class

--- Colllege_Management_System/src/Student.java
+++ Colllege_Management_System/src/Student.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Student extends Person {
     private ArrayList<String> courses;
 
-    public Student(String name, Long id, String email) {
+    public Student(String name, String id, String email) {
         super(name, id, email);
         courses = new ArrayList<>();
     }
