6124897d2d981be36d7ec2790923d90c7ef784b3
palexey2006 <palexey2006@gmail.com>
2026-03-15 13:02:21 -0400

    Created constructor for student class and ovveride method getDetails

--- Colllege_Management_System/src/Student.java
+++ Colllege_Management_System/src/Student.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
+
 public class Student extends Person {
+    private ArrayList<String> Courses;
+
+    public Student(String name, Long id, String email) {
+        super(name, id, email);
+        Courses = new ArrayList<>();
+    }
+
+    @Override
+    public void getDetails() {
+
+    }
 }
