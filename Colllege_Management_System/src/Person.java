17be03053c8ced54a54dbf5ee56fb64694523a87
palexey2006 <palexey2006@gmail.com>
2026-03-15 12:48:21 -0400

    created setters and getters for private fields in person class

--- Colllege_Management_System/src/Person.java
+++ Colllege_Management_System/src/Person.java
@@ -8,4 +8,28 @@ public abstract class Person {
         this.id = id;
         this.email = email;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
