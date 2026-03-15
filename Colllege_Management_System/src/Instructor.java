adcbbf26ea1e0a1449aa6907fc685c2a2d6bbea3
palexey2006 <palexey2006@gmail.com>
2026-03-15 17:53:29 -0400

    Created setter and getter for department field in Instructor class with regex validation

--- Colllege_Management_System/src/Instructor.java
+++ Colllege_Management_System/src/Instructor.java
@@ -1,10 +1,24 @@
 import java.util.ArrayList;
 
 public class Instructor extends Person{
-    ArrayList<String> courseTeaching;
+    private final ArrayList<String> courseTeaching;
     String department;
     public Instructor(String name, String id, String email, String department) {
         super(name,id,email);
+        if(!department.matches("^[A-Za-z ]+$")) {
+            throw new IllegalArgumentException("Invalid field of study! Example: Computer Science");
+        }
+        this.department = department;
+        courseTeaching = new ArrayList<>();
+    }
+
+    public String getDepartment() {
+        return this.department;
+    }
+    public void setDepartment(String department) {
+        if(!department.matches("^[A-Za-z ]+$")) {
+            throw new IllegalArgumentException("Invalid field of study! Example: Computer Science");
+        }
         this.department = department;
     }
 
