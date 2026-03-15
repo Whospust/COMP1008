ef86dcfeec1aaa99832192b7e95eed1310d1ae3e
palexey2006 <palexey2006@gmail.com>
2026-03-15 17:43:01 -0400

    Finished Student class: Added removeAllCourses method and field of study(getter and setter as well with validation)

--- Colllege_Management_System/src/Student.java
+++ Colllege_Management_System/src/Student.java
@@ -3,15 +3,29 @@ import java.util.NoSuchElementException;
 
 public class Student extends Person {
     private final ArrayList<String> courses;
+    private String major;
 
-    public Student(String name, String id, String email) {
+    public Student(String name, String id, String email, String major) {
         super(name, id, email);
         courses = new ArrayList<>();
+        this.major = major;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        if(!major.matches("^[A-Za-z ]+$")) {
+            throw new IllegalArgumentException("Invalid field of study! Example: Computer Science");
+        }
+        this.major = major;
     }
 
     @Override
     public void getDetails() {
         System.out.println("Hello, " + getName() + " #" + getId());
+        System.out.println("Study of field: " + major);
         if (!courses.isEmpty()) {
             System.out.println("Your course list:");
             for (String course : courses) {
@@ -26,13 +40,21 @@ public class Student extends Person {
         }
         courses.add(courseCode);
     }
+
     public void RemoveCourse(String courseCode) {
         if (!courseCode.matches("^[A-Z]{3}\\d{3}$")) {
             throw new IllegalArgumentException("Invalid course code! Example: CSE101");
         }
-        if(!courses.contains(courseCode)) {
+        if (!courses.contains(courseCode)) {
             throw new NoSuchElementException("Couldn't find a course " + courseCode + " in your course list.");
         }
         courses.remove(courseCode);
+    }
+
+    public void RemoveAllCourses() {
+        for (String course : courses) {
+            courses.remove(course);
+        }
+        System.out.println("You have successfully removed all courses!");
     }
 }
