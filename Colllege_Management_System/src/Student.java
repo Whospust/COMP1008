08baa12d29ca4c0f65d30c59d2efb4ac40ba45d1
palexey2006 <palexey2006@gmail.com>
2026-03-15 15:29:40 -0400

    added remove/add methods in student class

--- Colllege_Management_System/src/Student.java
+++ Colllege_Management_System/src/Student.java
@@ -1,7 +1,8 @@
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 
 public class Student extends Person {
-    private ArrayList<String> courses;
+    private final ArrayList<String> courses;
 
     public Student(String name, String id, String email) {
         super(name, id, email);
@@ -12,7 +13,7 @@ public class Student extends Person {
     public void getDetails() {
         System.out.println("Hello, " + getName() + " #" + getId());
         if (!courses.isEmpty()) {
-            System.out.println("Courses list:");
+            System.out.println("Your course list:");
             for (String course : courses) {
                 System.out.println(course);
             }
@@ -24,5 +25,14 @@ public class Student extends Person {
             throw new IllegalArgumentException("Invalid course code! Example: CSE101");
         }
         courses.add(courseCode);
+    }
+    public void RemoveCourse(String courseCode) {
+        if (!courseCode.matches("^[A-Z]{3}\\d{3}$")) {
+            throw new IllegalArgumentException("Invalid course code! Example: CSE101");
+        }
+        if(!courses.contains(courseCode)) {
+            throw new NoSuchElementException("Couldn't find a course " + courseCode + " in your course list.");
+        }
+        courses.remove(courseCode);
     }
 }
