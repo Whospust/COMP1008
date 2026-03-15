a8a2c59b8175b830ea4442f70e9a6c47ebf28abb
palexey2006 <palexey2006@gmail.com>
2026-03-15 12:44:24 -0400

    created constructor for Person class

--- Colllege_Management_System/src/Person.java
+++ Colllege_Management_System/src/Person.java
@@ -1,2 +1,11 @@
 public abstract class Person {
+    private String name;
+    private Long id;
+    private String email;
+
+    public Person(String name, Long id, String email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    }
 }
