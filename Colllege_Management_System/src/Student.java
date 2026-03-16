25d14c94f83fc0f243bb8bfd8aa6f0fce9763847
palexey2006 <palexey2006@gmail.com>
2026-03-16 14:55:50 -0400

    added comments for student class

--- Colllege_Management_System/src/Student.java
+++ Colllege_Management_System/src/Student.java
@@ -1,23 +1,31 @@
 import java.util.ArrayList;
 import java.util.NoSuchElementException;
-//TODO ADD COMMENTS EVERYWHERE
+
 public class Student extends Person {
+    // List that collects courses for each individual student
     private final ArrayList<String> courses;
+
+    // fiend of study of an individual student
     private String major;
 
     public Student(String name, String id, String email, String major) {
+        // calling parent constructor
         super(name, id, email);
         courses = new ArrayList<>();
+
+        // validating that fiend of study is only letters and spaces
         if(!major.matches("^[A-Za-z ]+$")) {
             throw new IllegalArgumentException("Invalid field of study! Example: Computer Science");
         }
         this.major = major;
     }
 
+    // standard getter
     public String getMajor() {
         return major;
     }
 
+    // standard setter with validation
     public void setMajor(String major) {
         if(!major.matches("^[A-Za-z ]+$")) {
             throw new IllegalArgumentException("Invalid field of study! Example: Computer Science");
@@ -25,6 +33,8 @@ public class Student extends Person {
         this.major = major;
     }
 
+
+    // overriding parent method from abstract class person
     @Override
     public void getDetails() {
         System.out.println("Student name: " + getName() + "Student id: #" + getId());
@@ -39,6 +49,7 @@ public class Student extends Person {
         }
     }
 
+    // method, which validates course and then adds it if its valid
     public void addCourse(String courseCode) {
         if (!courseCode.matches("^[A-Z]{3}\\d{3}$")) {
             throw new IllegalArgumentException("Invalid course code! Example: CSE101");
@@ -46,6 +57,7 @@ public class Student extends Person {
         courses.add(courseCode);
     }
 
+    // same method as above but to remove course with exactly the same regex validation
     public void RemoveCourse(String courseCode) {
         if (!courseCode.matches("^[A-Z]{3}\\d{3}$")) {
             throw new IllegalArgumentException("Invalid course code! Example: CSE101");
@@ -56,6 +68,7 @@ public class Student extends Person {
         courses.remove(courseCode);
     }
 
+    // removing all courses from student's list
     public void RemoveAllCourses() {
         for (String course : courses) {
             courses.remove(course);
