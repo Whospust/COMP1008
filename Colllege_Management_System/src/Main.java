ed1911dfc1dc30fa898727892e7950ca8276a8ab
palexey2006 <palexey2006@gmail.com>
2026-03-17 18:31:34 -0400

    started main class

--- Colllege_Management_System/src/Main.java
+++ Colllege_Management_System/src/Main.java
@@ -1,5 +1,12 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
+    private static ArrayList<Student> studentList = new ArrayList<>();
+    private static ArrayList<Instructor> instructorList = new ArrayList<>();
+    private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-//TODO ADD COMMENTS EVERYWHERE
+
     }
+
 }
