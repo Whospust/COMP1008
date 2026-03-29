84201af2673a4e5f507326fcb844bc28cea97be9
palexey2006 <palexey2006@gmail.com>
2026-03-29 10:26:54 -0400

    handled setOnAction method for subtractBtn

--- lab-5/src/CalculatorApp.java
+++ lab-5/src/CalculatorApp.java
@@ -40,6 +40,7 @@ public class CalculatorApp extends Application {
         VBox vbox = new VBox(20);
         vbox.getChildren().addAll(inputHbox,buttonsHbox,resultHbox);
         // Step 6: Event handling for buttons
+
         addBtn.setOnAction(e -> {
             String input1 = userInput1.getText();
             String input2 = userInput2.getText();
@@ -55,6 +56,23 @@ public class CalculatorApp extends Application {
                 }
             }
         });
+
+        subtractBtn.setOnAction(e -> {
+            String input1 = userInput1.getText();
+            String input2 = userInput2.getText();
+            if(input1.isEmpty() || input2.isEmpty()) {
+                result.setText("Input cannot be empty");
+            } else {
+                try {
+                    int num1 = Integer.parseInt(input1);
+                    int num2 = Integer.parseInt(input2);
+                    result.setText(String.valueOf(num1 - num2));
+                } catch (NumberFormatException err) {
+                    result.setText("Input must contain only numbers!");
+                }
+            }
+        });
+
         // Step 7: Create scene and show stage
         Scene scene = new Scene(vbox, 300, 350);
         primaryStage.setScene(scene);
